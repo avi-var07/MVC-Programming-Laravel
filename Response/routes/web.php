3b62c42849b7a99edf0cd75f39d3ce60d37fc87e
Aviral Varshney <aviralvarshney07@gmail.com>
2026-03-11 11:38:41 +0530

    Changed the web UI using Bootstap in Response

--- Response/routes/web.php
+++ Response/routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('aboutUs');
+    return view('welcome'); //also configured Welcome with bootstrap
 });
-Route::get('/AboutUs    ', function () {
+Route::get('/AboutUs', function () {
     return response()->view('aboutUs'); //chaining with view
 });
 Route::get('/download-file', function () {
@@ -20,10 +20,10 @@ Route::get('/admin', function () {
 
 Route::get('/hello', function () {
     return response("<h1 style='color:green'>Welcome to this World</h1>"); //added styling
-    });
+});
     
 Route::get('/json-response', function () {
     $data=["name"=>"Aviral", "age"=>"21"];
     return response()->json($data); //chaining with json
-    });
+});
         
